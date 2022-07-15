2ae6bc54cc0607d302326d44ee8fbf7726dee183
chuuyabott <108595724+chuuyabott@users.noreply.github.com>
2022-07-16 02:39:45 +0300

    Update config.js

--- config.js
+++ config.js
@@ -41,7 +41,7 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['967712989862'] //ur owner number
+global.owner = ['13239884856'] //ur owner number
 global.ownername = "chuuya"
 global.ytname = "YT: Xeon" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
@@ -55,8 +55,8 @@ global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n🦄🦄🦄 "
 global.author = "🦄Dream Guy Xeon"
 global.themeemoji = "🐶"
 global.reactmoji = "😘"
-global.ownertag = ['967712989862'] //ur owner tag
-global.ownernummenu = ['967712989862'] //ur owner number in menu and all
+global.ownertag = ['13239884856'] //ur owner tag
+global.ownernummenu = ['13239884856'] //ur owner number in menu and all
 global.watermark = "Xeon Bot Inc." //ur watermark
 global.botscript = "https://github.com/DGXeon/CheemsBot-MD3"
 global.linkz1 = "https://chat.whatsapp.com/HYj9wu5Jrv6CROxyeQbHoS"
